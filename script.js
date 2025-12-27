704a30ffdf813208e3c93df82b45ccc817d69fd3
astra-the-boop <astra.the.boop@icloud.com>
2025-12-28 04:38:39 +0700

    Fixed "day" in countdown label

--- script.js
+++ script.js
@@ -803,7 +803,7 @@ function loadTimelineBlocks(){
 
                 if(years>0) parts.push(`${years} year${years!== 1?"s":""}`);
                 if(months>0) parts.push(`${months} month${months!== 1?"s":""}`);
-                parts.push(`${days} day${days!==1?"s":""}`)
+                parts.push(`${remainingDays} day${remainingDays!==1?"s":""}`)
 
                 labelText += ` - ${parts.join(' ')}`;
             }
