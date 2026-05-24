a01b25f7a3cb5cc6f9405f4ebecc950254976ac5
zibuyin <natdrone101@gmail.com>
2026-05-24 10:57:47 +0100

    fix: confetti clicks

--- confetti.js
+++ confetti.js
@@ -1,25 +1,47 @@
 
 const defaults = {
-    particleCount: 200,
+    particleCount: 400,
     spread: 120,
     scalar: 1.7,
-    startVelocity: 80,
+    startVelocity: 70,
+    gravity: 10,
 }
 function doConfetti(){
-    // Left
-    confetti({
-    ...defaults,
-    angle: 45,
-    origin: { y: 1, x:0}
-    });
+    // // Left
+    // confetti({
+    // ...defaults,
+    // angle: 45,
+    // origin: { y: 1, x:0}
+    // });
 
-    // Right
+    // // Right
+    // confetti({
+    // ...defaults,
+    // angle: 135,
+    // // spread: 0,
+    // origin: { y: 1, x:1}
+    // });
+
+
     confetti({
-    ...defaults,
-    angle: 135,
-    // spread: 0,
-    origin: { y: 1, x:1}
-    });
+  spread: 360,
+  ticks: 200,
+  gravity: 1,
+  decay: .94,
+  startVelocity: 30,
+  particleCount: 100,
+  scalar: 3,
+  shapes: ["image"],
+  shapeOptions: {
+    image: [{
+        src: "https://particles.js.org/images/fruits/apple.png",
+        width: 32,
+        height: 32
+      },
+      
+    ]
+  }
+});
 }
 
 // onmousemove = function(e){doConfettiAtCursor(e.x, e.y)}
